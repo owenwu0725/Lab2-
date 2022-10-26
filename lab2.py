4832e174a4f13a0e8d108814a20600ff5d5f868f
owenwu0725 <89065110+owenwu0725@users.noreply.github.com>
2022-10-26 19:25:09 +0800

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,76 +1,115 @@
-table=["auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","stastic","struct","switch","typedef","union","unsigned","void","volatile","while"]
+import re
+import profile
+
+def fun():
 
 
-# number total keywords
-def get_key(infile):
-    global count
-    count = 0
-    for line in infile.readlines():    # 对每行元素操作
-        line = line.strip()         # 将每行字符串的头尾空行符去除
-        line = line.replace(",", " ").replace(".", " ").replace("{", " ").replace("}", " ").replace(":", " ").replace(";", " ").replace("?", " ").replace("(", " ").replace(")", " ").replace("!", " ").replace(">"," ").replace("<", " ").replace("=", " ")
-        words = line.split()    # 将每行按空格划分为单词，做成单词列表
-        for word in words:
-            for keyword in table:
-                if word == keyword:
-                    count = count+1
-    return count
+    key_word = ['auto', 'break', 'case', 'char', 'const',
+                'continue', 'default', 'do', 'double', 'else', 'enum',
+                'extern', 'float', 'for', 'goto', 'if', 'int', 'long',
+                'register', 'return', 'short', 'signed', 'sizeof', 'static',
+                'struct', 'switch', 'typedef', 'union',
+                'unsigned', 'void', 'volatile', 'while']  # All the keywords in C and C++ are generated here
+    file_name = input("Please input the file name:")
+    level = input("Please input the level(1-4):")  # Determine the name of the object file and the processing level
+    level = int(level)
+
+    with open(file_name) as C_file:
+        lines = C_file.readlines()
+        # open the object file and read it line by line
+
+    def first(lines):
+        count = 0
+        lists = []
+        for line in lines:
+            chars = ['(', ')', '{', '}', ':', ',', '<', '>', '=', '+', '-', '#', ';']
+
+        for line in lines:
+            line = re.sub(r'#.*$', "", line)
+            line = re.sub(r'//.*', "", line)
+            line = re.sub(r'".*"', "", line)
+            line = re.sub(r"'.*'", "", line)
+            # Converts special characters to Spaces, word segmentation
+            for bracket in chars:
+                line = line.replace(bracket, ' ')
+                key_line = line.split()
+
+            if 'else' in key_line and 'if' in key_line:  # Consider esle if as a whole to pave the way for the following function processing
+                lists.append('elif')
+                count += 2
+            else:
+                for word in key_line:
+                    if word in key_word:
+                        count += 1
+                        lists.append(word)
+        return lists, count
 
 
-# number switch&case
-def get_switch(infile):
-    global count_2, count_3
-    count_2 = 0
-    count_3 = 0
-    flag=0
-    cable=[]
-    for line in infile.readlines():  # 对每行元素操作
-        line = line.strip()  # 将每行字符串的头尾空行符去除
-        line = line.replace(",", " ").replace(".", " ").replace("{", " ").replace("}", " ").replace(":", " ").replace(";", " ").replace("?", " ").replace("(", " ").replace(")", " ").replace("!", " ").replace(">", " ").replace("<", " ").replace("=", " ")
-        words = line.split()  # 将每行按空格划分为单词，做成单词列表
-        for word in words:
-            if word == "switch":
-                count_2 = count_2+1
-                flag=1    # 有一个switch前提
-            elif word == "case" and flag == 1:
-                count_3 = count_3+1
-            elif word=="default":
-                cable.append(count_3)
-                count_3 = 0
-                flag = 0
-    print("switch num: %d" % count_2)
-    print("case num:")
-    length=len(cable)
-    index = 0
-    while index < length:
-        print(cable[index])
-        print(" ")
-        index += 1
+    def second(lists):
+        case_num = []
+        switch_num = 0
+        while True:
+            num = 0
+            if 'default' in lists:
+                place = lists.index('default')
+                switch_num += 1  # The number of SwitchCase structures is identified by the number of default
+                for word in lists[:place]:
+                    if word == 'case':
+                        num += 1
+                case_num.append(num)  # dentify the number of cases in each group
+                del lists[:place + 1]
+            else:
+                break
+        return case_num, switch_num
 
 
-# function mode selection
-def file_mode(path, mode):
-    infile = open(path,"r")
-    if mode == 1:
-        total=get_key(infile)
-        print("total num: %d" % total)
+    def third(lists):
+        if_else_num = 0
+        list = []
+        if_elif_num = 0
+        for word in lists:
+            count = 0
 
-    elif mode == 2:
-        total = get_key(infile)
-        print("total num: %d" % total)
-        get_switch(infile)
+            if word == 'if':
+                if_else_num += 1
 
-    infile.close()
+            if word == 'if' or word == 'elif':
+                list.append(word)
+            elif word == 'else':
+                while True:
+                    temp = list.pop()
+                    if temp == 'elif':
+                        count = 1
+                    elif temp == 'if':
+                        break
+            if count == 1:
+                if_elif_num += 1
+
+        for word in list:  # Consider that there is no else in if else if structure
+            if word == 'if':
+                if_else_num -= 1
+        return if_else_num, if_elif_num
+
+        # Call and execute the function
+
+
+    lists, total_num = first(lines)
+    case_nums, switch_nums = second(lists[:])
+    if_else_nums, if_elif_nums = third(lists[:])
+    if level >= 1:
+        print(f"total num:{total_num}")
+    if level >= 2:
+        print(f"switch num:{switch_nums}")
+        print("case num:", end=' ')
+        for case in case_nums:
+            print(case, end=' ')
+
+    if level >= 3:
+        print('\n'f"if-else num:{if_nums - if_elif_nums}")
+    if level >= 4:
+        print(f"if-elif_num:{if_elif_nums}")
+
     return 0
 
-
-# main function
-def main():
-    mode=input("please select the mode from 1 to 4:")
-    path_find=input("please input the test file path:")    # path for my computer is 'D:\Python\code file.txt'
-    mode_num=int(mode)
-    file_mode(path_find, mode_num)
-
-
-main()
-
+profile.run('fun()')
 
